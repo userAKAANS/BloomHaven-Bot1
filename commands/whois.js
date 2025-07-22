bd76f720351358b988a82562c97d1a4458bdd1f9
userAKAANS <besttieshello5@gmail.com>
2025-07-22 16:05:02 +0300

    Create whois.js

---
+++ commands/whois.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('whois')
+    .setDescription('Get the Discord tag of a user by their ID')
+    .addStringOption(opt =>
+      opt.setName('id')
+        .setDescription('The Discord user ID (e.g. 1234567890)')
+        .setRequired(true)),
+
+  async execute(interaction) {
+    const id = interaction.options.getString('id');
+
+    try {
+      const user = await interaction.client.users.fetch(id);
+      await interaction.reply({ content: `🧾 **User Tag:** ${user.tag}`, ephemeral: true });
+    } catch (err) {
+      console.error('❌ Error fetching user by ID:', err);
+      await interaction.reply({ content: '❌ Invalid ID or user not found.', ephemeral: true });
+    }
+  }
+};
